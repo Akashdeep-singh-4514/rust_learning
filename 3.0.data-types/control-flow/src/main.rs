8790579b12eecc006216ea023f687638aa17abfa
Akashdeep <akashdeep.singh@sifars.com>
2025-12-09 17:42:56 +0530

    Add conditional check for number equality in main.rs to handle case when number is equal to 5

--- 3.0.data-types/control-flow/src/main.rs
+++ 3.0.data-types/control-flow/src/main.rs
@@ -3,6 +3,8 @@ fn main() {
     let number=6;
     if number < 5 {
         println!("condition was true");
+    } else if number == 5 {
+        println!("condition was equal to 5");
     } else {
         println!("condition was false");
     }
