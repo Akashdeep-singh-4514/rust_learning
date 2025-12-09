9f9eff9eb606966b27c04845e3d31f0d923cfea7
Akashdeep <akashdeep.singh@sifars.com>
2025-12-09 18:06:50 +0530

    Add User struct implementation in main.rs with examples of instantiation, property access, and email update demonstration.

--- 6.0.struct/struct-or-object/src/main.rs
+++ 6.0.struct/struct-or-object/src/main.rs
@@ -1,3 +1,39 @@
 fn main() {
-    println!("Hello, world!");
+    #[derive(Debug)]
+    struct User {
+        username: String,
+        email: String,
+        sign_in_count: u64,
+        active: bool,
+    }
+    let user1 = User {
+        username: String::from("John"),
+        email: String::from("john@example.com"),
+        sign_in_count: 1,
+        active: true,
+    };
+    let mut user2 = User {
+        username: String::from("John"),
+        email: String::from("john@example.com"),
+        sign_in_count: 1,
+        active: true,
+    };
+    println!("=================USER1=================");
+    println!("User1 debug print: {:?}", user1);
+    println!("User1 pretty print: {:#?}", user1);
+    println!("=================USER1 PROPERTIES=================");
+    println!("User1 username: {}", user1.username);
+    println!("User1 email: {}", user1.email);
+    println!("User1 sign_in_count: {}", user1.sign_in_count);
+    println!("User1 active: {}", user1.active);
+
+    println!("=================befor USER 2 UPDATE EMAIL=================");
+    println!("User2 debug print: {:?}", user2);
+
+    println!("=================after update email: {}", user2.email);
+    user2.email = String::from("john@newexample.com");
+    println!("User2 debug print: {:?}", user2);
+
+
+
 }
