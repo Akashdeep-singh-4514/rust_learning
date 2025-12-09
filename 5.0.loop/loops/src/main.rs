851717b1591b78ea51f1ee719a969bf59b096d0c
Akashdeep <akashdeep.singh@sifars.com>
2025-12-09 17:42:25 +0530

    Add loop examples in main.rs demonstrating basic loop, loop with return value, and nested loop with break and continue. Also, include Cargo.lock for the 'loops' package.

--- 5.0.loop/loops/src/main.rs
+++ 5.0.loop/loops/src/main.rs
@@ -1,3 +1,39 @@
 fn main() {
-    println!("Hello, world!");
+
+    println!("=================LOOP=================");
+    let mut x = 0;
+    loop {
+        x += 1;
+        println!("x is {}", x);
+        if x == 10 {
+            break;
+        }
+    };
+    println!("=================LOOP with return value=================");
+    x = 0;
+    let result = loop {
+        x += 1;
+        println!("x is {}", x);
+        if x == 10 {
+            break x * x;
+        }
+    };
+    println!("The result is {result}");
+
+    println!("=================LOOP with continue=================");
+    x = 0;
+    loop {
+        x += 1;
+        if x == 10 {
+            continue;
+        }
+        println!("x is {}", x);
+        if x == 15 {
+            break;
+        }
+    }
+
+    println!("=================NESTED LOOP WITH BREAK AND CONTINUE=================");
+    let mut y = 0;
+
 }
