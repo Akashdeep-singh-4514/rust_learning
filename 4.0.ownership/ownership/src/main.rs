19d4a3a4d9b40c495f2b063bc8c3127e1b08f082
Akashdeep <akashdeep.singh@sifars.com>
2025-12-09 17:19:52 +0530

    Implement string ownership example in main.rs with calculate_length function

--- 4.0.ownership/ownership/src/main.rs
+++ 4.0.ownership/ownership/src/main.rs
@@ -1,3 +1,14 @@
 fn main() {
-    println!("Hello, world!");
+    // create a new string usig String::from
+    let s = String::from("hello");
+    let length = calculate_length(&s);
+    println!("The length of '{}' is {}.", s, length);
+    // s2 takes ownership of s and  s is no longer valid
+    let s2 = s;
+    let length2 = calculate_length(&s2);
+    println!("The length of '{}' is {}.", s2, length2);
+}
+
+fn calculate_length(some_string: &String) -> usize {
+    return some_string.len();
 }
