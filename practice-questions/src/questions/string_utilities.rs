78af4a15e3aef22f8556a7c84d88910971192031
Akashdeep singh <waliaakash95@gmail.com>
2026-01-03 18:48:15 +0530

    conver strig to lower case

--- practice-questions/src/questions/string_utilities.rs
+++ practice-questions/src/questions/string_utilities.rs
@@ -12,7 +12,17 @@ fn is_palindrome(input: &str) -> bool {
     return input == reverse(input);
 }
 
-fn to_snake_case(input: &str) {}
+fn to_snake_case(input: &str)->String {
+    let mut snake=String::new();
+    for i in input.chars(){
+        if i.is_uppercase(){
+            snake=format!("{}_{}",snake,i.to_lowercase());
+        }else{
+            snake=format!("{}{}",snake,i);
+        }
+    }
+    snake
+}
 pub fn run() {
     let input = inputs::str_inputs().replace("\n", "");
     let rev = reverse(&input);
@@ -23,4 +33,5 @@ pub fn run() {
     // println!("input",input);
     println!("Reverse: {}", rev);
     println!("is palindrome: {}", palindrome);
+    println!("snake cased: {}", to_snake_case(&input));
 }
